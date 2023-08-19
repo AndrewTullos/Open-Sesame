0864aeea6de60a50f3ec73dade805448021ee3e9
Andrew Tullos <tullos.andrew@gmail.com>
2023-08-18 19:01:16 -0500

    more if statements saving to an object

--- Develop/script.js
+++ Develop/script.js
@@ -8,30 +8,54 @@
 // Either displayed on an alert or writte to the page
 // Write password to the #password input
 
-// This Code generates the length requested by the user
-const generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener('click', function () {
-  const textPromptOne = prompt('Enter how many characters your password should be?', 'Between 8 - 128')
-  if (textPromptOne < 8) {
-    return alert('Dude! No, that is is less than 8. Try again...')
-  } else if (textPromptOne > 128) {
-    return alert('Easy Tiger! That\'s too long! Try again!')
-  } else {
-    const passwordLength = textPromptOne
-    console.log(passwordLength)
-    // const promptAnswerOne = document.getElementById('password')
-    // promptAnswerOne.innerText = textPromptOne
-  }
-  // This code determines case sensitivity and special chars/nums
-  const passwordUp = confirm('Would you like uppercase?');
-  console.log(passwordUp);
-  const passwordLow = confirm('Would you like lowercase?');
-  console.log(passwordLow);
-  const passwordSpecial = confirm('Would you like special characters?');
-  console.log(passwordSpecial);
-  alert('Sweet dude, one sec!')
-});
+// Arrays
+// const lowAlphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+// const upperAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+// const specialChar = '!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'.split('');
 
+// Password Array
+// const password = [
+
+// ]
+
+// This Code generates the length requested by the user
+const passObj = {
+  length: undefined,
+  special: undefined,
+  upper: undefined,
+  lower: undefined,
+};
+
+console.log(passObj)
+
+const generateBtn = document.querySelector('#generate');
+generateBtn.addEventListener('click', function () {
+  const textPromptOne = prompt('Enter how many characters your password should be?', 'Between 8 - 128');
+  if (Number(textPromptOne) < 8) {
+    return alert('Not enough. Try again')
+  } else if (Number(textPromptOne) > 128) {
+    return alert('Too many. Try again')
+  } else {
+    passObj.length = Number(textPromptOne)
+  };
+
+  const specialChar = confirm('Will your password require special characters?');
+  if (specialChar === true) {
+    passObj.special = true;
+  };
+  const upperAlphabet = confirm('Will your password require upper case?');
+  if (upper === true) {
+    passObj.upper = true;
+  };
+  const lowAlphabet = confirm('Will your password require lower case??');
+  if (lower === true) {
+    passObj.lower = true;
+  };
+})
+
+
+
+// //Take passwordLength, passwordUp, passwordLow and passwordSpec
 
 
 // function writePassword() {
@@ -44,3 +68,16 @@ generateBtn.addEventListener('click', function () {
 
 // // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
+
+
+
+// Junk
+// const textPromptOne = prompt('Enter how many characters your password should be?', 'Between 8 - 128')
+// if (textPromptOne < 8) {
+//   return alert('Dude! No, that is is less than 8. Try again...')
+// } else if (textPromptOne > 128) {
+//   return alert('Easy Tiger! That\'s too long! Try again!')
+// } else {
+//   const passwordLength = textPromptOne
+//   console.log(passwordLength)
+// }
