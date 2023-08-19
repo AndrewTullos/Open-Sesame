d992a517a5b266fe3eef06f952da6b96c9f614b1
Andrew Tullos <tullos.andrew@gmail.com>
2023-08-18 20:02:14 -0500

    for loops added to render the password

--- Develop/script.js
+++ Develop/script.js
@@ -8,23 +8,22 @@
 // Either displayed on an alert or writte to the page
 // Write password to the #password input
 
-// Can't remember what this is for? Revisit?
-// Arrays
-// const lowAlphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-// const upperAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-// const specialChar = '!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'.split('');
-
 // This Code generates the length requested by the user
+
+// Object stores the password parameters set by the user
 const passObj = {
   length: undefined,
   special: undefined,
   upper: undefined,
   lower: undefined,
 };
+// User instructions
+// alert('Press the generat button to create a password!')
 
-
+// Button generates the code to fill the object
 const generateBtn = document.querySelector('#generate');
 generateBtn.addEventListener('click', function () {
+  // Determines the length of requested password
   const textPromptOne = prompt('Enter how many characters your password should be?', 'Between 8 - 128');
   if (Number(textPromptOne) < 8) {
     return alert('Not enough. Try again')
@@ -34,46 +33,52 @@ generateBtn.addEventListener('click', function () {
     passObj.length = Number(textPromptOne)
   };
 
+  // Determines if password should include special characters
   const specialChar = confirm('Will your password require special characters?');
   if (specialChar === true) {
     passObj.special = true;
   };
+
+  // Determines if password should include uppercase
   const upper = confirm('Will your password require upper case?');
   if (upper === true) {
     passObj.upper = true;
   };
+
+  // Determines if password should include lower case
   const lower = confirm('Will your password require lower case??');
   if (lower === true) {
     passObj.lower = true;
   };
-})
 
-console.log(passObj)
+  // function writePassword() {
+  //   const password = generatePassword();
+  //   const passwordText = document.querySelector("#password");
+  // }
 
+  // for ()
 
-// //Take passwordLength, passwordUp, passwordLow and passwordSpec
-
-
-// function writePassword() {
-//     const password = generatePassword();
-//     const passwordText = document.querySelector("#password");
-
-//     passwordText.value = password;
+});
 
 //   }
 
 // // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
 
+// Can't remember what this is for? Revisit?
+// Arrays
+const lowAlphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+for (i = 0, i < lowAlphabet.length, i++) {
+  console.log(lowAlphabet[i]);
+}
+
+const upperAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+for (i = 0, i < upperAlphabet.length, i++) {
+  console.log(upperAlphabet[i]);
+}
 
 
-// Junk
-// const textPromptOne = prompt('Enter how many characters your password should be?', 'Between 8 - 128')
-// if (textPromptOne < 8) {
-//   return alert('Dude! No, that is is less than 8. Try again...')
-// } else if (textPromptOne > 128) {
-//   return alert('Easy Tiger! That\'s too long! Try again!')
-// } else {
-//   const passwordLength = textPromptOne
-//   console.log(passwordLength)
-// }
+const specialChar = '!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'.split('');
+for (i = 0, i < specialChar.length, i++) {
+  console.log(specialChar[i]);
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Password Generator Starter Code
 https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_prompt
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
